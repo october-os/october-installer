3c6b8ab80295c1063e894e8778b03fafb192a6b9
efournierrobert <elliott1447@gmail.com>
2025-12-13 15:57:46 -0500

    Documented error

--- installer/pkg/user/errors.go
+++ installer/pkg/user/errors.go
@@ -2,14 +2,20 @@ package user
 
 import "fmt"
 
+// UserInstantiationError represents an error that occured during the
+// instantiation of a user inside the constructor.
+//
+// It wraps an error for better debugging.
 type UserInstantiationError struct {
 	err error
 }
 
+// Error returns a formatted string containing the error message.
 func (e UserInstantiationError) Error() string {
 	return fmt.Sprintf("failed to instantiate user: error=%v", e.err.Error())
 }
 
+// Unwrap unwraps the error inside UserIntantiationError
 func (e UserInstantiationError) Unwrap() error {
 	return e.err
 }
