8e609b6e0b2ae3177e3c36072f90c572ca350ed1
efournierrobert <elliott1447@gmail.com>
2025-12-13 15:54:25 -0500

    Docs and small refactor

--- installer/pkg/user/user.go
+++ installer/pkg/user/user.go
@@ -1,3 +1,6 @@
+// Package user provides the struct representing a user that
+// needs to be created and the functions to create it in the newly
+// installed system.
 package user
 
 import (
@@ -8,6 +11,7 @@ import (
 	"github.com/arch-couple/arch-couple-installer/pkg/arch_chroot"
 )
 
+// User represents a user that needs to be created.
 type User struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
@@ -15,6 +19,13 @@ type User struct {
 	Sudoer   bool   `json:"sudoer"`
 }
 
+// Constructor for User struct.
+//
+// If homepath is empty, it will be set to: /home/[username]
+//
+// Will either return:
+//   - *User
+//   - UserInstantiationError: When an incorrect value is passed
 func New(username, password, homepath string, sudoer bool) (*User, error) {
 	if strings.TrimSpace(username) == "" || strings.TrimSpace(password) == "" {
 		return nil, UserInstantiationError{
@@ -38,34 +49,57 @@ func New(username, password, homepath string, sudoer bool) (*User, error) {
 	}, nil
 }
 
-func CreateUsers(users []User) error {
-	err := setupSudoerFile()
+// Takes in a user then creates it in the newly installed system.
+//
+// Errors that can be returned:
+//   - PipeError
+//   - ArchChrootError
+func CreateUsers(user User) error {
+	err := userAdd(user.Username, user.Homepath)
 	if err != nil {
 		return err
 	}
 
-	for _, user := range users {
-		err = userAdd(user.Username, user.Homepath)
+	err = setPassword(user.Username, user.Password)
+	if err != nil {
+		return err
+	}
+
+	if user.Sudoer {
+		err = addToSudoer(user.Username)
 		if err != nil {
 			return err
-		}
-
-		err = setPassword(user.Username, user.Password)
-		if err != nil {
-			return err
-		}
-
-		if user.Sudoer {
-			err = addToSudoer(user.Username)
-			if err != nil {
-				return err
-			}
 		}
 	}
 
 	return nil
 }
 
+// Adds the wheel user group as system admins in /etc/sudoers
+// inside the newly installed system.
+//
+// Errors that can be returned:
+//   - PipeError
+//   - ArchChrootError
+func SetupSudoerFile() error {
+	wheelLine := "%wheel      ALL=(ALL:ALL) ALL"
+	command := fmt.Sprintf("echo \"%s\" >> /etc/sudoers", wheelLine)
+
+	err := arch_chroot.Run(command)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Adds the user with the given username to the wheel group to make
+// it a sudoer inside the newly installed system. Make sure to run
+// SetupSudoerFile() before running this.
+//
+// Errors that can be returned:
+//   - PipeError
+//   - ArchChrootError
 func addToSudoer(username string) error {
 	addToWheel := fmt.Sprintf("usermod -aG wheel %s", username)
 
@@ -77,6 +111,12 @@ func addToSudoer(username string) error {
 	return nil
 }
 
+// Runs useradd with the given username and homepath inside the newly
+// installed system.
+//
+// Errors that can be returned:
+//   - PipeError
+//   - ArchChrootError
 func userAdd(username, homepath string) error {
 	createCommand := fmt.Sprintf("useradd -m %s -d %s", username, homepath)
 
@@ -88,20 +128,14 @@ func userAdd(username, homepath string) error {
 	return nil
 }
 
+// Sets the given user password for the given password inside
+// the newly installed system.
+//
+// Errors that can be returned:
+//   - PipeError
+//   - ArchChrootError
 func setPassword(username, password string) error {
 	command := fmt.Sprintf("echo %s | passwd %s -s", password, username)
-
-	err := arch_chroot.Run(command)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func setupSudoerFile() error {
-	wheelLine := "%wheel      ALL=(ALL:ALL) ALL"
-	command := fmt.Sprintf("echo \"%s\" >> /etc/sudoers", wheelLine)
 
 	err := arch_chroot.Run(command)
 	if err != nil {
