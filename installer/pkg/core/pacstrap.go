c9edd5c00e1c50d0381ffa9f1fc09971fa1d4d74
efournierrobert <elliott1447@gmail.com>
2025-12-22 04:27:48 -0500

    Did core pacstrap function

--- installer/pkg/core/pacstrap.go
+++ installer/pkg/core/pacstrap.go
@@ -1,1 +1,33 @@
 package core
+
+import (
+	"errors"
+	"os/exec"
+)
+
+const LINUX_KERNEL string = "linux"
+const BASE_ARCH string = "base"
+const BASE_LINUX_FIRMWARE string = "linux-firmware"
+
+func InstallBasicInstallation() error {
+	cpuMicrocode, err := GettCpuMicroCode()
+	if err != nil {
+		return CoreInstallError{
+			Err: err,
+		}
+	} else if cpuMicrocode == "" {
+		return CoreInstallError{
+			Err: errors.New("Unsupported CPU detected. Needs to be an AMD or Intel X86_64 CPU."),
+		}
+	}
+
+	cmd := exec.Command("pacstrap", "-K", "/mnt", BASE_ARCH, LINUX_KERNEL, BASE_LINUX_FIRMWARE, cpuMicrocode)
+
+	if err := cmd.Run(); err != nil {
+		return CoreInstallError{
+			Err: err,
+		}
+	}
+
+	return nil
+}
