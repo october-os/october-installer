f4bfa330366282b6eba0b60aebc58abe026c8525
efournierrobert <elliott1447@gmail.com>
2025-12-31 17:07:02 -0500

    Added doc and moved errors into public function

--- installer/pkg/mirrors/errors.go
+++ installer/pkg/mirrors/errors.go
@@ -2,14 +2,20 @@ package mirrors
 
 import "fmt"
 
+// MirrorListError represents an error that occured
+// when setting up and saving the mirrorlist file.
 type MirrorListError struct {
 	err error
 }
 
+// Error returns a formatted error message containing the
+// original error message inside.
 func (e MirrorListError) Error() string {
 	return fmt.Sprintf("mirrorlist error: error=%s", e.err.Error())
 }
 
+// Unwrap returns the original error wrapped inside
+// MirrorListError.
 func (e MirrorListError) Unwrap() error {
 	return e.err
 }
--- installer/pkg/mirrors/mirrors.go
+++ installer/pkg/mirrors/mirrors.go
@@ -8,12 +8,20 @@ import (
 	"strings"
 )
 
+// Absolute path to the mirrorlist file.
 const mirrorlistFile string = "/etc/pacman.d/mirrorlist"
 
+// Sets the mirrorlist file with only the servers for the
+// given countries and removes all the unused ones.
+//
+// Can return error types:
+//   - MirrorListError
 func SetMirrorList(countries []string) error {
 	mirrorMap, err := getMirrors()
 	if err != nil {
-		return err
+		return MirrorListError{
+			err: err,
+		}
 	}
 
 	fullCountrySlice := make([]string, 0, len(mirrorMap))
@@ -22,27 +30,33 @@ func SetMirrorList(countries []string) error {
 	}
 
 	if err := areCountriesValid(countries, fullCountrySlice); err != nil {
-		return err
-	}
-
-	return saveMirrorlist(countries, mirrorMap)
-}
-
-func saveMirrorlist(countries []string, mirrorMap map[string][]string) error {
-	file, err := os.Create(mirrorlistFile)
-	if err != nil {
 		return MirrorListError{
 			err: err,
 		}
+	}
+
+	if err := saveMirrorlist(countries, mirrorMap); err != nil {
+		return MirrorListError{
+			err: err,
+		}
+	}
+
+	return nil
+}
+
+// Saves all the servers of the given countries inside the
+// mirrorlist file.
+func saveMirrorlist(countries []string, mirrorMap map[string][]string) error {
+	file, err := os.Create(mirrorlistFile)
+	if err != nil {
+		return err
 	}
 	defer file.Close()
 
 	for _, country := range countries {
 		for _, server := range mirrorMap[country] {
 			if _, err := file.WriteString(server + "\n"); err != nil {
-				return MirrorListError{
-					err: err,
-				}
+				return err
 			}
 		}
 	}
@@ -50,24 +64,24 @@ func saveMirrorlist(countries []string, mirrorMap map[string][]string) error {
 	return nil
 }
 
+// Checks if the given countries are inside the given full country list.
 func areCountriesValid(countries []string, fullCountryList []string) error {
 	for _, country := range countries {
 		if !slices.Contains(fullCountryList, country) {
-			return MirrorListError{
-				err: fmt.Errorf("country %s not present in mirrors", country),
-			}
+			return fmt.Errorf("country %s not present in mirrors", country)
 		}
 	}
 
 	return nil
 }
 
+// Reads the mirrorlist file and returns a map
+// that has the country name as the key and a slice of
+// all the servers as the value.
 func getMirrors() (map[string][]string, error) {
 	file, err := os.Open(mirrorlistFile)
 	if err != nil {
-		return nil, MirrorListError{
-			err: err,
-		}
+		return nil, err
 	}
 	defer file.Close()
 
