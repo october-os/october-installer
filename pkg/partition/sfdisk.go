b5c31b4b8fc0230b39fd9caf2ec968f63ec81f74
efournierrobert <elliott1447@gmail.com>
2026-01-20 16:15:09 -0500

    Added a function that reverse the partitions if they are reversed

--- pkg/partition/sfdisk.go
+++ pkg/partition/sfdisk.go
@@ -20,6 +20,7 @@ type SfdiskJsonPartitionTable struct {
 // SfdiskJsonPartition represents one element of the 'partitions' field/array of SfdiskJsonPartitionTable
 type SfdiskJsonPartition struct {
 	Node string `json:"node"`
+	Uuid string `json:"uuid"`
 }
 
 // Gets a drive's state using 'sfdisk --json <device>'
