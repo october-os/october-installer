600bca5e60a5f4750148bdd987fc71affa1a2329
eFournierRobert <elliott1447@gmail.com>
2026-04-07 15:35:34 -0400

    Doc for keyring

--- pkg/keyring/keyring.go
+++ pkg/keyring/keyring.go
@@ -4,8 +4,15 @@ import (
 	"github.com/october-os/october-installer/pkg/utils"
 )
 
+// commandExecutor is the command executor for the keyring
+// package.
 var commandExecutor = utils.NewCommandExecutor
 
+// UpdateKeyRing updates the current Arch Linux keyring of the ISO
+// so it can install October even if it is out of date.
+//
+// Can return error of type:
+// 	- KeyringUpdateError
 func UpdateKeyRing() error {
 	cmd := commandExecutor("pacman", "-Sy", "--noconfirm", "archlinux-keyring")
 	if err := cmd.Run(); err != nil {
