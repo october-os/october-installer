ce173482c4dd60531ab5038bac7f334ed489486e
Elliott Fournier-Robert <67513792+eFournierRobert@users.noreply.github.com>
2026-04-14 16:00:47 -0400

    73 create unit tests for keyring package (#77)
    * udpated keyring to use command executor
    
    * Unit tests for keyring package
    
    * Doc for keyring
    
    * Doc update for keyring

--- pkg/keyring/keyring.go
+++ pkg/keyring/keyring.go
@@ -1,10 +1,21 @@
 package keyring
 
-import "os/exec"
+import (
+	"github.com/october-os/october-installer/pkg/utils"
+)
 
+// commandExecutor is the command executor for the keyring
+// package.
+var commandExecutor = utils.NewCommandExecutor
+
+// UpdateKeyRing updates the current Arch Linux keyring of the ISO
+// so it can install October even if it is out of date.
+//
+// Can return error of type:
+//   - KeyringUpdateError
 func UpdateKeyRing() error {
-	cmd := exec.Command("pacman", "-Sy", "--noconfirm", "archlinux-keyring")
-	if err := cmd.Start(); err != nil {
+	cmd := commandExecutor("pacman", "-Sy", "--noconfirm", "archlinux-keyring")
+	if err := cmd.Run(); err != nil {
 		return KeyringUpdateError{err: err}
 	}
 	return nil
