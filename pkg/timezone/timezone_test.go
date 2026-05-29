0782461ebefcc5872a41b558695f552cbfa0f79a
efournierrobert <elliott1447@gmail.com>
2026-05-29 09:37:48 -0400

    Rest of timezones tests

--- pkg/timezone/timezone_test.go
+++ pkg/timezone/timezone_test.go
@@ -44,6 +44,49 @@ func TestSetTimeError(t *testing.T) {
 	assert.NotNil(t, err)
 }
 
+func TestSetHwClock(t *testing.T) {
+	originalExecutor := arch_chroot.CommandExecutor
+	arch_chroot.CommandExecutor = mocks.NewCommandExecutorMock
+	defer func() {
+		arch_chroot.CommandExecutor = originalExecutor
+		mocks.CommandExecutorGot = []string{}
+	}()
+
+	err := SetHwClock()
+	assert.Nil(t, err)
+	assert.Len(t, mocks.CommandExecutorGot, 1)
+	assert.Equal(
+		t,
+		archChrootCommandBuilder("hwclock --systohc"),
+		mocks.CommandExecutorGot[0])
+}
+
+func TestSetHwClockError(t *testing.T) {
+	originalExecutor := arch_chroot.CommandExecutor
+	arch_chroot.CommandExecutor = mocks.NewCommandExecutorMock
+	mocks.ReturnError = true
+	defer func() {
+		arch_chroot.CommandExecutor = originalExecutor
+		mocks.CommandExecutorGot = []string{}
+		mocks.ReturnError = false
+	}()
+
+	err := SetHwClock()
+	assert.NotNil(t, err)
+}
+
+func TestValidateTimezone(t *testing.T) {
+	timezone := "America/Montreal"
+	err := ValidateTimezone(timezone)
+	assert.Nil(t, err)
+}
+
+func TestValidateInvalidTimezone(t *testing.T) {
+	timezone := "America/Mascouche"
+	err := ValidateTimezone(timezone)
+	assert.NotNil(t, err)
+}
+
 func archChrootCommandBuilder(command string) string {
 	return fmt.Sprintf("/usr/bin/arch-chroot /mnt /bin/bash -c %s", command)
 }
